26e0834084353b85f523d1166985f931d7f2f080
rafael <rafaeleupinheiro@gmail.com>
2019-11-08 11:25:42 -0300

    A little more about JSX

--- src/index.js
+++ src/index.js
@@ -2,4 +2,36 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 const element = document.getElementById('root')
-ReactDOM.render(<h1>Hello Word</h1>, element)
+// ReactDOM.render(<h1>Hello Word</h1>, element)
+
+// Using JSX
+ReactDOM.render(
+    <ul>
+        <li>1) Pedro </li>
+        <li>2) Maria </li>
+        <li>3) Ana </li>
+    </ul>
+ , element)
+
+
+ // Using Javascript
+// const list = document.createElement('ul')
+
+// var item = document.createElement('li')
+// var text = document.createTextNode('1) Predo')
+// item.appendChild(text)
+// list.appendChild(item)
+
+// var item = document.createElement('li')
+// var text = document.createTextNode('2) Maria')
+// item.appendChild(text)
+// list.appendChild(item)
+
+// var item = document.createElement('li')
+// var text = document.createTextNode('3) Ana')
+// item.appendChild(text)
+// list.appendChild(item)
+
+// const element = document.getElementById('root')
+// element.appendChild(list)
+
